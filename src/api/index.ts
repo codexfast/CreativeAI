3d81050732fdd87dcbe3baaca2bb443ef975678b
Gilberto <codexfast@gmail.com>
2025-05-16 17:08:27 -0300

    some changes

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,18 +2,27 @@
  * API Configuration File
  */
 
-// const PROTOCOL = "http://"
-// const HOST = "192.168.1.65"
-// const PORT = "8000"
+const PROTOCOL = process.env.PROTOCOL || "http://";
+const HOST = process.env.HOST || "192.168.1.65";
+const PORT = process.env.PORT || "8000";
 
-const PROTOCOL = "https://"
-const HOST = "b32e-34-16-162-100.ngrok-free.app"
-const PORT = null
-// 
-
-const BASE_URL = `${PROTOCOL}${HOST}:${PORT?? ""}`;
+let BASE_URL;
+if (process.env.PROD === "colab") {
+    BASE_URL = "";
+} else {
+    const PORT_STR = PORT ? `:${PORT}` : "";
+    BASE_URL = `${PROTOCOL}${HOST}${PORT_STR}`;
+}
 
 export const OUTPUT_URL = `${BASE_URL}/output/`;
 export const DOWNLOAD_OUTPUT_URL = `${BASE_URL}/download/`;
 export const GALLERY_URL = `${BASE_URL}/gallery/`;
 export const API_URL = `${BASE_URL}/api`;
+
+// Optional: export as default object
+export default {
+    OUTPUT_URL,
+    DOWNLOAD_OUTPUT_URL,
+    GALLERY_URL,
+    API_URL
+};
